24dd3a6cfc1e91f3b8b0dc02aa13f1e35d16adea
gustafssonjohanna <johannakristina96@hotmail.com>
2024-05-15 12:18:56 +0200

    RecyclerViewAdapter added, content in progress

---
+++ app/src/main/java/com/example/project/RecyclerViewAdapter.java
@@ -0,0 +1,67 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Plants> items;
+    private LayoutInflater layoutInflater;
+    private OnClickListener onClickListener;
+
+    RecyclerViewAdapter(Context context, List<Plants> items, OnClickListener onClickListener) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.items = items;
+        this.onClickListener = onClickListener;
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.plant_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(items.get(position).getName());
+        holder.location.setText(items.get(position).getLocation());
+        /*holder.size.setText(items.get(position).getSize()+" m");*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+        TextView location;
+        TextView size;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.tv_title);
+            location = itemView.findViewById(R.id.tv_location);
+            size = itemView.findViewById(R.id.tv_size);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(items.get(getAdapterPosition()));
+        }
+    }
+
+    public interface OnClickListener {
+        void onClick(Plants item);
+    }
+}
